f685f01a0c72ff0466c3ca4da59447ecb96d8324
John Smith <xfgryujk@126.com>
2020-03-03 22:18:00 +0800

    减小编译后文件尺寸

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import {
+  Button, Dialog, Form, FormItem, Input, Option, Select, Switch, Tabs, TabPane, Tooltip, Upload
+} from 'element-ui'
 import * as hooks from './hooks'
 import * as gui from './gui'
 
 function main () {
-  Vue.use(ElementUI)
+  // 初始化element
+  Vue.use(Button)
+  Vue.use(Dialog)
+  Vue.use(Form)
+  Vue.use(FormItem)
+  Vue.use(Input)
+  Vue.use(Option)
+  Vue.use(Select)
+  Vue.use(Switch)
+  Vue.use(Tabs)
+  Vue.use(TabPane)
+  Vue.use(Tooltip)
+  Vue.use(Upload)
+
   hooks.init()
   gui.init()
 }
